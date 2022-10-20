af23defca0d92251df856942b04454fa1f8cec5f
Aman Dhyani <102688642+Aman-Dhyani@users.noreply.github.com>
2022-10-20 04:58:49 -0700

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -9,9 +9,6 @@ export default function Home() {
             <div className='main-container'>
                 <p>a react-applicstion where you can play music and youtube video</p>
                 <div id='header' >
-                    <a href='https://github.com/Aman-Dhyani'>
-                        <img id='refImage' src="https://avatars.githubusercontent.com/u/102688642?v=4" alt="error" />
-                    </a>
                     <h4>Welcome To My Mideo</h4>
                 </div>
                 <VideoPlayer />
