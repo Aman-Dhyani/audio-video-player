b961fa1e93c1ed7d73c8577da07425c719f88e12
Aman Dhyani <102688642+Aman-Dhyani@users.noreply.github.com>
2022-10-19 22:34:51 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './Components/Home/Home'
 import Video from './Components/Videos/vid files/Video'
 import MusicData from './Components/Musics/local imported music/MusicData'
@@ -7,14 +7,14 @@ import MusicData from './Components/Musics/local imported music/MusicData'
 export default function App() {
   return (
     <>
-      <BrowserRouter>
+      <Router >
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/video' element={<Video />} />
           <Route path='/musicdata' element={<MusicData />} />
           <Route path='*' element={<h1>page not found</h1>} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </>
   )
 }
